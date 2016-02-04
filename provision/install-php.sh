7718076acb26e3df8cd18fc4a9d0da91a4578330
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2016-02-04 02:08:23 -0200

    Removing comments

--- provision/install-php.sh
+++ provision/install-php.sh
@@ -4,10 +4,8 @@ echo " "
 echo " ----- INSTALLING PHP REQUIREMENTS ----- "
 echo " "
 
-#add-apt-repository ppa:ondrej/php-7.0 > /dev/null 2>&1
 sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php
 apt-get -qq update
-#apt-get -y install php5 php5-curl > /dev/null 2>&1
 sudo apt-get purge php5-common -y
 sudo apt-get -y install php7.0 php7.0-curl > /dev/null 2>&1
 sudo apt-get -y --purge autoremove
